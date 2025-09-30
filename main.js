4ae612a1a33e6522195212bde5fe38fa8d114baf
amandamachado1 <145574744+amandamachado1@users.noreply.github.com>
2025-09-30 11:50:00 -0300

    upload

--- main.js
+++ main.js
@@ -5,24 +5,102 @@
 
 const perguntas = [
     {
-        enunciado: "Pergunta?",
+        enunciado: "O que mais te atrai em uma profissão?",
         alternativas: [
             {
-                texto: " Texto/resposta",
-                afirmacao: " Afirmação ",
+                texto: "Cuidar das pessoas, melhorar a vida delas.",
+                afirmacao: "Você tende para Saúde (Medicina, Enfermagem, Psicologia, Nutrição, Fisioterapia, Odontologia).",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: "Resolver problemas práticos e criar soluções.",
+                afirmacao: "Você tende para Exatas e Engenharias (Mecânica, Engenharia Mecânica, Software, Administração).",
             }
         ]
     },
     {
+        enunciado: "Em que ambiente você se sente mais motivado?",
+        alternativas: [
+            {
+                texto: "Ao ar livre, com contato com a natureza",
+                afirmacao: "Perfil de Ciências Agrárias (Agronomia, Zootecnia, Biologia, Agricultura).",
+            },
+            {
+                texto: "Em escritórios, hospitais ou laboratórios.",
+                afirmacao: "Perfil de Ciências da Saúde e Humanas (Medicina, Direito, Psicologia, Ciências Contábeis, Farmácia).",
+            }
+        ]
+    },
+    {
+        enunciado: "O que você prefere?",
+        alternativas: [
+            {
+                texto: "Lidar com pessoas, comunicação e liderança.",
+                afirmacao: "Perfil de Humanas (Educação, Artes Cênicas, Administração, Direito).",
+            },
+            {
+                texto: " Lidar com máquinas, sistemas ou cálculos.",
+                afirmacao: "Perfil de Exatas (Software, Engenharia, Mecânica, Contabilidade).",
+            }
+        ]
+    },
+    {
+        enunciado: "Como você aprende melhor?",
+        alternativas: [
+            {
+                texto: "Com prática, experimentos e movimento.",
+                afirmacao: "Você se encaixa em Saúde, Educação Física ou Agrárias.",
+            },
+            {
+                texto: "Com leitura, teoria e pesquisa.",
+                afirmacao: "Você se encaixa em Direito, Psicologia, Ciências Biológicas.",
+            }
+        ]
+    },
+    {
+        enunciado: "Se tivesse que liderar um projeto, escolheria:",
+        alternativas: [
+            {
+                texto: "Um projeto social (bem-estar, inclusão, saúde).",
+                afirmacao: "Perfil de Saúde/Humanas.",
+            },
+            {
+                texto: "Um projeto tecnológico (máquinas, sistemas, inovação).",
+                afirmacao: "Perfil de Exatas/Tecnologia.",
+            }
+        ]
+    },
+        {
+        enunciado: "Como você gosta de resolver problemas?",
+        alternativas: [
+            {
+                texto: "Conversando e ouvindo diferentes pontos de vista.",
+                afirmacao: "Perfil de Psicologia, Direito, Administração.",
+            },
+            {
+                texto: "Testando, calculando e aplicando soluções práticas.",
+                afirmacao: "Perfil de Engenharia, Software, Mecânica.",
+            }
+        ]
+    },
+        {
+        enunciado: "Em relação à criatividade, você prefere:",
+        alternativas: [
+            {
+                texto: " Expressar ideias por meio de arte, fala ou movimento.",
+                afirmacao: "Perfil de Artes Cênicas, Comunicação, Educação Física.",
+            },
+            {
+                texto: "Criar soluções técnicas, inventar coisas novas.",
+                afirmacao: "Perfil de Engenharia, Agronomia, Software.",
+            }
+        ]
+    },
+        {
         enunciado: " Pergunta?",
         alternativas: [
             {
                 texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                afirmacao: "Afirmação",
             },
             {
                 texto: " Texto/resposta ",
@@ -30,12 +108,12 @@ const perguntas = [
             }
         ]
     },
-    {
+        {
         enunciado: " Pergunta?",
         alternativas: [
             {
                 texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                afirmacao: "Afirmação",
             },
             {
                 texto: " Texto/resposta ",
@@ -43,20 +121,7 @@ const perguntas = [
             }
         ]
     },
-    {
-        enunciado: " Pergunta?",
-        alternativas: [
-            {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
-            },
-            {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
-            }
-        ]
-    },
-    {
+        {
         enunciado: " Pergunta?",
         alternativas: [
             {
